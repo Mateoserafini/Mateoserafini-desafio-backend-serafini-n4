f88b49fd7e6e0db32124282755ab48a7c6e70563
Mateo Serafini <matuserafini@gmail.com>
2024-03-28 16:45:12 -0300

    Desafio terminado antes de el test

---
+++ src/app.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import { ProductManager } from './controllers/productManager.js';
+import { CartManager } from './controllers/cartManager.js';
+import { productsRouter } from './routes/products.router.js';
+import { cartsRouter } from './routes/cart.router.js';
+import { viewsRouter } from './routes/views.router.js';
+import { engine } from 'express-handlebars';
+import { Server } from 'socket.io';
+import http from 'http'; 
+import socketProducts from './socket/socketProducts.js';
+
+const app = express();
+const PORT = 8080;
+
+export const productManager = new ProductManager();
+export const cartManager = new CartManager();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('./public'));
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './src/views');
+
+app.use('/api/products', productsRouter);
+app.use('/api/carts', cartsRouter);
+app.use('/', viewsRouter);
+
+const server = http.createServer(app); // Crea el servidor HTTP utilizando Express
+const socketServer = new Server(server); // Pasa el servidor HTTP a Socket.IO
+
+server.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
+
+socketProducts(socketServer);
---
+++ src/controllers/productManager.js
@@ -0,0 +1,86 @@
+import { promises as fs } from 'fs';
+import { v4 as uuidv4 } from 'uuid';
+
+export class ProductManager {
+    static ultId = 0;
+
+    constructor() {
+        this.path = './src/models/products.json';
+        this.products = [];
+    }
+
+    async addProduct({ title, description, stock, img, code, price  }) {
+        try {
+            const id = uuidv4();
+            let newProduct = { id, title, description, stock, img, code, price };
+
+            this.products = await this.getProducts();
+            this.products.push(newProduct);
+
+            await fs.writeFile(this.path, JSON.stringify(this.products));
+            return newProduct;
+        } catch (error) {
+            throw new Error('Error al añadir producto: ' + error.message);
+        }
+    }
+
+    async getProducts() {
+        try {
+            const response = await fs.readFile(this.path, 'utf-8');
+            const responseJSON = JSON.parse(response);
+            return responseJSON;
+        } catch (error) {
+            throw new Error('Error al obtener productos: ' + error.message);
+        }
+    }
+
+    async getProductsById(id) {
+        try {
+            const response = await this.getProducts();
+            const product = response.find(product => product.id == id);
+
+            if (product) {
+                return product;
+            } else {
+                throw new Error('Producto no encontrado');
+            }
+        } catch (error) {
+            throw new Error('Error al obtener producto por ID: ' + error.message);
+        }
+    }
+
+    async updateProduct(id, { ...data }) {
+        try {
+            const products = await this.getProducts();
+            const index = products.findIndex(product => product.id == id);
+
+            if (index !== -1) {
+                products[index] = { id, ...data };
+                await fs.writeFile(this.path, JSON.stringify(products));
+                return products[index];
+            } else {
+                throw new Error('Producto no encontrado');
+            }
+        } catch (error) {
+            throw new Error('Error al actualizar producto: ' + error.message);
+        }
+    }
+
+    async deleteProduct(id) {
+        try {
+            const products = await this.getProducts();
+            const index = products.findIndex(product => product.id == id);
+    
+            if (index !== -1) {
+                products.splice(index, 1);
+                await fs.writeFile(this.path, JSON.stringify(products));
+            } else {
+                throw new Error('Producto no encontrado');
+            }
+        } catch (error) {
+            throw new Error('Error al eliminar producto: ' + error.message);
+        }
+    }
+    
+}
+
